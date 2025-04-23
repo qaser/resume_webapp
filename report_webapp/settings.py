4ca47398338b32af6b36ff42359d15242733b4fc
Alexey S <huji@mail.ru>
2025-04-24 00:00:43 +0500

    fix

--- report_webapp/settings.py
+++ report_webapp/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ee05p0c4=qch8r%c=(*cnyzpd!t(w7v3n%fml=f!#p(ggyw@#5'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['212.193.48.114', 'localhost', '127.0.0.1']
 
 
 INSTALLED_APPS = [
